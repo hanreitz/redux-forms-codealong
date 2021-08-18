a699e27f0897ba8ff5a5b467072cbfa258df9c7a
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-08-18 16:21:06 -0400

    commit tree 72a63722f1ea5073cff3bbdc3a62bb2e739dd6e2

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CreateTodo extends Component {
+  constructor(){
+    super()
+    this.state ={
+      text: ''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.dispatch({ type: 'ADD_TODO', todo: this.state.text })
+    this.setState({
+      text: ''
+    })
+  }
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <p>
+            <label>Add To-Do </label>
+            <input type="text" onChange={event => this.handleChange(event)} value={this.state.text} />
+          </p>
+          <input type="submit" />
+        </form>
       </div>
     )
   }
 }
 
-export default CreateTodo;
+export default connect()(CreateTodo);
